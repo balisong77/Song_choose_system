14a5f0ca3a79be7e761659e136c2124cb0b6fb7b
zlq <1085789239@qq.com>
2019-07-26 22:26:47 +0800

    v_1.3

--- src/utils/jdbcUtils.java
+++ src/utils/jdbcUtils.java
@@ -10,18 +10,22 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class jdbcUtils {
-
+    private static ComboPooledDataSource cds;
+    static {
+        cds = new ComboPooledDataSource("mysql");
+    }
 
     public static Connection getConnection() throws SQLException {
-        DataSource dataSource = new ComboPooledDataSource("mysql");
-        Connection connection = dataSource.getConnection();
-        System.out.println(connection);
+//        DataSource dataSource = new ComboPooledDataSource("mysql");
+        Connection connection = cds.getConnection();
+//        System.out.println(connection);
         return connection;
     }
     public static void closeConnection(Connection connection){
         if(connection != null){
             try {
                 connection.close();
+//                System.out.println(connection);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
